05077d4f9739024000da59ad702f2e023de65921
Sixtus Anyanwu <sikky606@gmail.com>
2021-10-01 17:49:17 +0100

    made use of useEffect to trigger events

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import BlogList from './BlogList';
 
 const Home = () => {
@@ -12,9 +12,20 @@ const Home = () => {
         const newblogs = blogs.filter(blog => (blog.id !== id))
         setBlogs(newblogs) // Set the new state as the array of filtered blogs.
     }
+
+    const [name, setName] = useState('Mario') //declare a use state hook
+    const [num, setNum] = useState(4)
+
+    useEffect(() => {
+        console.log("Random shit...")
+    }, [name, num]) // this useEffect runs if there are dependencies attached to it.
+
     return(
         <div className="home">
            <BlogList blogs={blogs} title="All Blogs" handleDelete = {handleDelete} />
+           <button onClick={() => setName('Luigi')}>Change useEffect Hook</button>
+           <p>{ name } is {num + 2} years old.</p>
+           <button onClick={() => setNum(6)}>Change num.</button>
         </div>
     )
 }
