e9ec99859ece25feec1d92c80325aa4a75b88f1d
Sixtus Anyanwu <sikky606@gmail.com>
2021-10-03 12:13:47 +0100

    added a pending usestate hook to simulate time between fetch

--- src/Home.js
+++ src/Home.js
@@ -2,22 +2,27 @@ import { useState, useEffect } from 'react'
 import BlogList from './BlogList';
 
 const Home = () => {
-    const [blogs, setBlogs] = useState(null)
+    const [blogs, setBlogs] = useState(null) // resct use state hooks
+    const [isPending, setisPending] = useState(true)
 
     useEffect(() => {
-        fetch('http://localhost:8000/blogs')
-        .then(res => {
-            return res.json()
-        })
-        .then(data => {
-           setBlogs(data) 
-        })
-    })
+        setTimeout(() => { // add a a delay of 2 seconds before fetching data
+            fetch('http://localhost:8000/blogs')
+            .then(res => {
+                return res.json()
+            })
+            .then(data => {
+            setBlogs(data) 
+            setisPending(false)
+            })
+        }, 2000)
+    }, [])
      // this useEffect runs on load if there are dependencies attached to it.
         // Evalutes if blog and blog components are both true. If one is false, nothing renders.
     return(
         <div className="home"> 
-           {blogs && <BlogList blogs={blogs} title="All Blogs" />}
+            { isPending && <div> Loading... </div> } 
+           { blogs && <BlogList blogs={blogs} title="All Blogs" /> }
         </div>
     ) 
 }
