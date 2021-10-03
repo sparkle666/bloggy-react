fe735fb4fcaf16bb2bd1d9f244127434bf8a7217
Sixtus Anyanwu <sikky606@gmail.com>
2021-10-03 11:48:45 +0100

    added some dynamic json data using json-server

--- src/Home.js
+++ src/Home.js
@@ -2,31 +2,23 @@ import { useState, useEffect } from 'react'
 import BlogList from './BlogList';
 
 const Home = () => {
-    const [blogs, setBlogs] = useState([
-        {title: "My first title", body: "Lorem ipsum somento willyon", author: "Sparkles", id: 1},
-        {title: "Welcome to my blog", body: "Lorem ipsum somento willyon", author: "James", id: 2},
-        {title: "Some really cool blog", body: "Lorem ipsum somento...", author: "Elija", id: 3},
-    ])
-
-    const handleDelete = (id) => {
-        const newblogs = blogs.filter(blog => (blog.id !== id))
-        setBlogs(newblogs) // Set the new state as the array of filtered blogs.
-    }
-
-    const [name, setName] = useState('Mario') //declare a use state hook
-    const [num, setNum] = useState(4)
+    const [blogs, setBlogs] = useState(null)
 
     useEffect(() => {
-        console.log("Random shit...")
-    }, [name, num]) // this useEffect runs if there are dependencies attached to it.
-
+        fetch('http://localhost:8000/blogs')
+        .then(res => {
+            return res.json()
+        })
+        .then(data => {
+           setBlogs(data) 
+        })
+    })
+     // this useEffect runs on load if there are dependencies attached to it.
+        // Evalutes if blog and blog components are both true. If one is false, nothing renders.
     return(
-        <div className="home">
-           <BlogList blogs={blogs} title="All Blogs" handleDelete = {handleDelete} />
-           <button onClick={() => setName('Luigi')}>Change useEffect Hook</button>
-           <p>{ name } is {num + 2} years old.</p>
-           <button onClick={() => setNum(6)}>Change num.</button>
+        <div className="home"> 
+           {blogs && <BlogList blogs={blogs} title="All Blogs" />}
         </div>
-    )
+    ) 
 }
 export default Home;
